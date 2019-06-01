cf861d56a0a3794b29f4a81c743c29ebedc6ab30
PhoenixDeveloper <mefisto40000x@gmail.com>
2019-06-02 01:37:05 +0300

    Завершение разработки игры "Змейка"

--- Snake/Snake/Snake.cs
+++ Snake/Snake/Snake.cs
@@ -40,6 +40,19 @@ namespace Snake
             return NextPoint;
         }
 
+        internal bool IsHitTail()
+        {
+            var head = pList.Last();
+            for (int i = 0; i < pList.Count - 2; i++)
+            {
+                if (head.IsHit(pList[1]))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
         public void HandleKey(ConsoleKey key)
         {
             if (key == ConsoleKey.LeftArrow)
